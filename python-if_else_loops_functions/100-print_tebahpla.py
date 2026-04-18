8d2f5151c156322312190b28cb21d39c3b7e7928
isi232 <elizadeismayil10@gmail.com>
2026-04-18 19:20:52 +0000

    ..

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    print("{}{}".format(chr(122 - (25 - i)), chr(89 - (25 - i))), end="")
+for i in range(26):
+    c = 122 - i
+    if i % 2 == 0:
+        print("{}".format(chr(c)), end="")
+    else:
+        print("{}".format(chr(c - 32)), end="")
