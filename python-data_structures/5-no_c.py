e2e13624dbdb06a199ae40d9b2f32ad7cbb29aab
isi232 <elizadeismayil10@gmail.com>
2026-05-02 09:07:13 +0000

    Can you C me now?

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     result = ""
     for char in my_string:
-        if char != "c" or char != "C":
+        if char != "c" and char != "C":
             result += char
     return result
