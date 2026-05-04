0e0aa5728dbc1cf42947d6a839f0d2b1fab312f5
isi232 <elizadeismayil10@gmail.com>
2026-05-04 21:12:40 +0000

    Unique addition

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    total = 0
+    seen = []
+    for num in my_list:
+        if num not in seen:
+            total += num
+            seen.append(num)
+    return total
