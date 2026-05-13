64ecba5b65b100096b67c6a7eccf36a681721c14
isi232 <elizadeismayil10@gmail.com>
2026-05-13 22:23:20 +0000

    .

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,6 +7,5 @@ class MyList(list):
 
     def print_sorted(self):
         """Prints the list sorted in ascending order"""
-        sorted_list = sorted(self)
-        print(sorted_list)
-        return sorted_list
+        print(sorted(self))
+        return sorted(self)
