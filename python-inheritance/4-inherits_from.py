5b604cef341f0414feecbfe1abb72bf393829fbb
isi232 <elizadeismayil10@gmail.com>
2026-05-13 23:08:03 +0000

    inheritance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""inherits_from module"""
+
+
 def inherits_from(obj, a_class):
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    return type(obj) is not a_class and isinstance(obj, a_class)
