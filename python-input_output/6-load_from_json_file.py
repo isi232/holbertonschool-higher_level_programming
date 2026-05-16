ca9248d476c49b2f019f58160f1875f7a3bc4c08
isi232 <elizadeismayil10@gmail.com>
2026-05-16 00:27:23 +0000

    input_output

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that provides a function to create a Python object from a JSON file."""
+"""Module that provides a function to create an object from a JSON file."""
 import json
 
 
