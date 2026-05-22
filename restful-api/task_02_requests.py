134d5ba0e08a1ea95b9b2722d91a4e7e7e5e4b70
isi232 <elizadeismayil10@gmail.com>
2026-05-22 10:25:57 +0000

    restful-api

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/posts"
+    )
+
+    print("Status Code:", response.status_code)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        for post in posts:
+            print(post["title"])
+
+
+def fetch_and_save_posts():
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/posts"
+    )
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        data = []
+
+        for post in posts:
+            data.append({
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            })
+
+        with open("posts.csv", "w", newline="") as file:
+            writer = csv.DictWriter(
+                file,
+                fieldnames=["id", "title", "body"]
+            )
+
+            writer.writeheader()
+            writer.writerows(data)
