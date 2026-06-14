51381e4e1ee1a128d0d5e13473f4947e99e9a9c4
isi232 <elizadeismayil10@gmail.com>
2026-06-14 23:44:44 +0000

    python_comprehesion

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python3
-"""Module for measuring runtime."""
+"""
+Create a measure_time function with integers n
+and max_delay as arguments that measures the total
+ execution time for wait_n(n, max_delay), and returns
+ total_time / n.
+"""
+
+
 import asyncio
 import time
 
-wait_n = __import__('1-async_comprehension').wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> float:
-    """Measure the total execution time for wait_n and return average."""
+def measure_time(n: int, max_delay: int = 0) -> float:
+    """
+    Create a measure_time function with integers n and max_delay as
+    arguments that measures the total execution time for
+     wait_n(n, max_delay)
+
+    Args:
+    n: int
+    max_delay: int = 10
+
+    Returns:
+    total_time / n
+    """
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
     total_time = end_time - start_time
+
     return total_time / n
